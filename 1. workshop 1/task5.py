b224a0e5c9cbb32a5ee00120422944c381a5a8c8
AbubakirovRA <AbubakirovRA@ya.ru>
2022-01-22 11:05:18 +0500

    add change task5

--- 1. workshop 1/task5.py
+++ 1. workshop 1/task5.py
@@ -1,9 +1,16 @@
 # Написать программу вычисления значения функции sin(x)/x
-def f(x):
-    import math
-    return math.sin(x)
+# def f(x):
+    # import math
+    # return math.sin(x)
 
-def data_request():
+from math import * # данное выражение полностью импортирует библиотеку math
+# что позволяет далее вызывать математические функции не обращаясь к библиотеке
+# в каждом выражении с математической функцией
+
+def f(x):
+    return sin(x)
+
+def request_number():
     while True:
         try:
             num=float(input('enter float or integer '))
@@ -11,4 +18,4 @@ def data_request():
         except ValueError:
             print('You entered not float or integer! One more!')
 
-print('Result of sin function = ', f(data_request()))
+print('Result of sin function = ', f(request_number()))
