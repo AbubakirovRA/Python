f2641adfface7f13c927e5a264e8b0ac1e74596f
AbubakirovRA <AbubakirovRA@ya.ru>
2022-01-31 11:59:57 +0500

    add check_degree

--- 1. workshop 1/create_file.py
+++ 1. workshop 1/create_file.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 def new_file():
     name_file = input('enter name file ')+'.txt'
     with open(name_file, 'w', encoding='utf-8') as file:
         file.write('')
     return name_file
-=======
-def create_file():
-    with open('New_file.txt', 'w', encoding='utf-8') as my_file:
-        my_file.write('')
-
-create_file()
->>>>>>> ebe51ded323f43b388aeaa179b4dd8a856ff1554
