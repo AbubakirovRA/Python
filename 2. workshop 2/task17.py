b189758017e8e1664826b5576c64f32f6ae60792
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-01 22:04:02 +0500

    add task17 solution

--- 2. workshop 2/task17.py
+++ 2. workshop 2/task17.py
@@ -6,9 +6,23 @@ def get_list(n):
     list = [i for i in range(-n, n+1)] # использование генератора списка для заполнения
     return list
 
-def read_file():
-    file_name = input('enter filename')+'.txt'
+def read_file(file_name):
     with open(file_name, 'r', encoding='utf-8') as f:
         string_list = f.read().splitlines()
     return string_list
 
+def get_multipli(file_name, n):
+    positions = read_file(file_name)
+    elements = get_list(n)
+    multipli = 1
+    for i in positions:
+        try:
+            multipli *= int(elements[int(i)])
+        except IndexError:
+            continue
+    return multipli
+        
+
+file_name = input('enter filename: ')+'.txt'
+n = int(input('enter N - digit: '))
+print(f'multipli of elements for position: {get_multipli(file_name, n)}')
