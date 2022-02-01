be8622a205d6a9bcc4fb1f98f44985dd3219ff46
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-01 14:14:21 +0500

    Add task24 solution

--- 2. workshop 2/task24.py
+++ 2. workshop 2/task24.py
@@ -1,33 +1,20 @@
-# 24 В заданном списке вещественных чисел найдите разницу 
-# между максимальным и минимальным значением дробной части элементов. 
+# 24 В заданном списке вещественных чисел найдите разницу
+# между максимальным и минимальным значением дробной части элементов.
 # Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-# complete = [1.1, 1.2, 3.1, 5.2553, 10.015]
-
-# def Max(arr):
-#     max = arr[0]
-#     i = 1
-#     while i < len(arr):
-#         if arr[i] > max:
-#             max = arr[i]
-#         i+=1
-#     return max
-
-# def Min(arr):
-#     min = arr[0]
-#     i = 1
-#     while i < len(arr):
-#         if arr[i] < min:
-#             min = arr[i]
-#         i+=1
-#     return min
+def fractional_difference(list):
+    min = max = list[0] - int(list[0])
+    for i in list:
+        dif = i - int(i)
+        if  dif == 0:
+            continue
+        if dif > max:
+            max = dif
+        if dif < min:
+            min = dif
+    return max-min
 
 
-# def Difference(arr,x):
-#     fraction = []
-#     for i in arr:
-#         fraction.append(round(i-int(i),x))
-#     return (Max(fraction)- Min(fraction))
-
-# print(Difference(complete, 4))
+list = [1.1, 1.2, 3.1, 5, 10.01]
+print(round(fractional_difference(list), 2))
 
