fd76f6381a0e47c88d97289ba2a0b5a0cb87e21e
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-03 19:06:33 +0500

    add task33

--- 1. workshop 1/RND.py
+++ 1. workshop 1/RND.py
@@ -1,4 +1,7 @@
 # Вывод случайного числа
 import random
-a = random.randint(1, 4)
+a = random.randint(1, 3)
 print(a)
+# 1 - Рустем
+# 2 - Марианна
+# 3 - Ирек
---
+++ 2. workshop 2/task33.py
@@ -0,0 +1,32 @@
+# 33. Дана последовательность чисел.
+# Получить список уникальных элементов заданной последовательности.
+
+def get_unique(list):
+    i = 0
+    unique = []
+    while i < len(list):
+        if check_element(list, list[i], i):
+            unique.append(list[i])
+            i +=1
+        else:
+            i +=1
+        
+    return unique
+
+def check_element(list, element, count):
+    exception_index = []
+    i= count +1
+    while i < len(list):
+        if i == len(list)-1:
+            if list[i] == element:
+                return False
+            else:
+                return True
+        elif list[i] == element:
+            exception_index.append(i)
+        i +=1
+
+list = [1, 4, 7, 8, 10, 3, 4, 7, 1, 7, 1]
+print(get_unique(list))
+
+
