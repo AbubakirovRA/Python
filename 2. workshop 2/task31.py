32f005cd279c8aefc02066bfb83554ac44838bdc
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-06 22:43:47 +0500

    add task31

--- 2. workshop 2/task31.py
+++ 2. workshop 2/task31.py
@@ -4,16 +4,26 @@
 import math
 
 def prime_factors(num):
-    for i in range(2, num + 1):
-        if num % i == 0:
-            count = 1
-            h = (i//2 + 1)
-            for j in range(2, h):
-                if i % j == 0:
-                    count = 0
-                    break
-            if count == 1:
-                print(i)
+    while num % 2 == 0: #even number divisible
+        print (2)
+        num = num / 2
+    for i in range(3, int(math.sqrt(num+1)), 2): #n became odd
+        while num % i == 0:
+            print (i)
+            num = num / i
+    if num > 2:
+        print(int(num))
+
+    # for i in range(2, num + 1):
+    #     if num % i == 0:
+    #         count = 1
+    #         h = (i//2 + 1)
+    #         for j in range(2, h):
+    #             if i % j == 0:
+    #                 count = 0
+    #                 break
+    #         if count == 1:
+    #             print(i)
 
 num = int(input("Enter the number for calculating the prime factors :\n"))
 prime_factors(num)
