8833ad493943f2cef439f570d2157b08cbf708d0
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-06 22:38:04 +0500

    add task31

--- 2. workshop 2/task31.py
+++ 2. workshop 2/task31.py
@@ -1,2 +1,19 @@
 # Составить список простых множителей натурального числа N
+# https://www.pythonpool.com/prime-factorization-python/
 
+import math
+
+def prime_factors(num):
+    for i in range(2, num + 1):
+        if num % i == 0:
+            count = 1
+            h = (i//2 + 1)
+            for j in range(2, h):
+                if i % j == 0:
+                    count = 0
+                    break
+            if count == 1:
+                print(i)
+
+num = int(input("Enter the number for calculating the prime factors :\n"))
+prime_factors(num)
