ca258436420c988de8c46e4065bcba445e6dfabb
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-06 21:42:49 +0500

    add task20, 27, 28, 29

--- 2. workshop 2/task20.py
+++ 2. workshop 2/task20.py
@@ -1,6 +1,6 @@
 # Определить, присутствует ли в заданном списке строк, некоторое число
 
-str_list = ['q', 'wqqq', 'e', 'r', 'rr1', 'tt', 'qf', 'e', 'tt', 'y']
+str_list = ['q', 'wqqq', 'e', 'r', 'rr1', 'tt', 'q2f', 'e', 't4t', 'y']
 num = (input('enter number to find: '))
 
 def check_enter(str_list, num):
