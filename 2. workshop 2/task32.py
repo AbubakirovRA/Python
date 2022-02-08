c24d5b42748de85af467beb11b5e0e0ad61d338f
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-08 13:14:18 +0500

    Add changes  task32, 43

--- 2. workshop 2/task32.py
+++ 2. workshop 2/task32.py
@@ -2,9 +2,9 @@
 #     Получить список неповторяющихся элементов исходной последовательности
 #     Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
 
-list1 = [1, 2, 3, 0, 5, 1, 5, 14, 26, 155, 3, 10]
-# list2 = sorted(list1)
+list1 = [1, 4, 8, 10, 3, 4, 7, 1, 1, 4 , 5, 1, 2]
 
+# list2 = sorted(list1)
 # for i in reversed(range(len(list2)-1)):
 #     if list2[i+1] > list2[i]:
 #         continue
@@ -12,19 +12,16 @@ list1 = [1, 2, 3, 0, 5, 1, 5, 14, 26, 155, 3, 10]
 #         list2.remove(list2[i])
 # print(f'{list1} => {list2}')
 
-list2=[]
-k = 1
-for j in range(len(list1)-1):
-    for i in range(len(list1)-1):
-        try:
-            if list1[j] == list1[i+k]:
-                k += 1
-                break
-        except ValueError:         
-            if i == len(list1)-1:
-                list2.append(list1[j])
-                k += 1
-            else:
-                continue
-            k = 0
-print(f'{list1} => {list2}') 
+list2 = []
+for i in range(len(list1)):
+    if i == 0:
+        list2.append(list1[i])
+        continue
+    for j in range(len(list2)):
+        if list1[i] == list2[j]:
+            break
+        elif j == len(list2) - 1:
+            list2.append(list1[i])
+print(f'{list1} => {list2}')
+
+
